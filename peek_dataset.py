f0c100fcb759488adf307d769f170362129e6ad6
Joo Hyeon Park <hyeon@HYEONPRO.local>
2026-04-12 01:42:46 +0900

    Remove leftover experiment scripts

--- peek_dataset.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-data = np.load('/Users/hyeon/Projects/openh_gemini/basin_dataset.npz', allow_pickle=True)
-print("Keys:", data.files)
-print("X shape:", data['X'].shape)
-print("mid shape:", data['mid'].shape)
-print("rule_labels[:10]:", data['rule_labels'][:10])
-print("t0_indices[:10]:", data['t0_indices'][:10])
